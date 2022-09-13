c83ae35f182d0b057c1f6b72a8aae599b49bb099
raizo07 <agadaraizo@gmail.com>
2022-09-13 06:43:11 +0100

    edits

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xBdB01751a46A37d452227bf3aa3F2845BB5b2E74";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
